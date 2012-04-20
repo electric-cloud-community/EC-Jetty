6a9bfa8819ce5e22abd0acc0ee3c14ea5823965f
rsanchez <a@b>
2012-04-20 05:39:41 +0200

    Jetty procedure name(s) were changed in the step picker section
    [git-p4: depot-paths = "//depot/nimbus/integrations/EC-Jetty/main/": change = 59655]

--- project/ec_setup.pl
+++ project/ec_setup.pl
@@ -14,5 +14,7 @@ my %DeployApp = (
 
 $batch->deleteProperty("/server/ec_customEditors/pickerStep/EC-Jetty - RunJetty");
 $batch->deleteProperty("/server/ec_customEditors/pickerStep/EC-Jetty - DeployApp");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/Jetty - Run Jetty");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/Jetty - Deploy Application");
 
 @::createStepPickerSteps = (\%RunJetty, \%DeployApp);
